7d7bc1df1a16f321c0e28665e83f8dca043234cb
manhcuong1011 <22070311@vnu.edu.vn>
2025-09-23 13:54:44 +0700

    Create index.php

---
+++ week2/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $name = "Mr. A";
+        $age = 20;
+        $courses = array ("Java","C","PHP");
+        echo "Name: " . $name . ", age: ".$age . 
+        "<br/>3rd course is: " . $courses[2];
+        ?>
+    </body>
+</html>
